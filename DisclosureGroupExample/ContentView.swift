b2fe1ec679de2e633cd143f9c72bb14289f54df5
hasanali <h.alisiseci@gmail.com>
2022-02-03 23:33:32 +0300

    Initial Commit

---
+++ DisclosureGroupExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DisclosureGroupExample
+//
+//  Created by Hasan Ali Şişeci on 3.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DisclosureGroupExample/DisclosureGroupExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisclosureGroupExampleApp.swift
+//  DisclosureGroupExample
+//
+//  Created by Hasan Ali Şişeci on 3.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct DisclosureGroupExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
