4c6ae0933da894ffd87d05fe6da7d74f220bd0d9
hasanali <h.alisiseci@gmail.com>
2022-02-04 15:58:05 +0300

    content view edit

--- DisclosureGroupExample/ContentView.swift
+++ DisclosureGroupExample/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
+        ScrollView {
+            BasicDisclosureGroupView()
+            Divider()
             CustomDisclosureGroupView()
+        }
     }
 }
 
